0050d6b4c13fb9c1aea8aedc64f8253b7224b435
ElenaS35 <helena0518savitskaya@gmail.com>
2022-07-11 13:05:19 +0200

    my first commit

---
+++ js/skript_responsive.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots: true,
+        speed: 200,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+            breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            },
+            {
+            breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+            breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
